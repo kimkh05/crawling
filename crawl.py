6589659199d73c04c7e98e39c32d838789c92704
김경호 <81161675+kimkh05@users.noreply.github.com>
2022-11-28 18:07:13 +0900

    feat: get information

--- crawl.py
+++ crawl.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import requests
+from bs4 import BeautifulSoup as bs
+import matplotlib.pyplot as plt
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+url_list = []
+company_name_list = []
+
+def getList():
+  url = "https://www.wanted.co.kr/wdlist/518?country=kr&job_sort=company.response_rate_order&years=-1&locations=all"
+  res = requests.get(url)
+  
+getList()
+
