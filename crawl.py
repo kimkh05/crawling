43b544a34acefedee27145a99327ef6286f2d383
kimkh05 <nestjs01@naver.com>
2022-12-09 11:09:26 +0900

    using python collection counter

--- crawl.py
+++ crawl.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import csv
 import time
+from collections import Counter as cn
 
 url = "https://www.wanted.co.kr/wdlist/518?country=kr&job_sort=company.response_rate_order&years=-1&locations=all"
 options = webdriver.ChromeOptions()
@@ -22,14 +23,13 @@ driver.get(url)
 prev_height = driver.execute_script("return document.body.scrollHeight")
 
 # 반복 수행
-# while True:
-for _ in range(5):
+while True:
 # 스크롤 가장 아래로 내림
   driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
   time.sleep(2)
   curr_height = driver.execute_script("return document.body.scrollHeight")
-  # if curr_height == prev_height:
-  #   break
+  if curr_height == prev_height:
+    break
   prev_height = curr_height
 
 
@@ -39,7 +39,7 @@ res = driver.page_source
 
 soup = (bs(res, 'html.parser').select(".job-card-position"))
 
-title = []
+title: str = []
 for data in soup:
   title.append(data.get_text())
 
@@ -59,41 +59,14 @@ job = {"프론트엔드": 0,
        "풀스택": 0,
        "AI": 0,
        "기타" : 0}
-for lower_t in title:
-  t = lower_t.lower()
-  print(f'{t}')
-  if '프론트엔드' in t:
-    job["프론트엔드"] += 1
-    print("frontend")
-  elif '백엔드' in t:
-    job["백엔드"] += 1
-    print("backend")
-  elif '안드로이드' in t:
-    job["안드로이드"] += 1
-    print("android")
-  elif 'ios' in t:
-    job["IOS"] += 1
-    print("ios")
-  elif 'dev ops' in t:
-    job["Dev-Ops"] += 1
-    print("dev-ops")
-  elif 'linux' and 'window' in t:
-    job["OS"] += 1
-    print("OS")
-  elif '임베디드' in t:
-    job["임베디드"] += 1
-    print("임베디드")
-  elif "full-stack" in t:
-    job['풀스택'] += 1
-    print("풀스택")
-  elif 'ai'in t:
-    job["AI"] += 1
-    print("AI")
-  elif '웹' and '앱' in t:
-    job["풀스택"] += 1
-    print("웹 앱")
-  else :
-    job["기타"] += 1
-    print("기타")
 
-job
+value_title = cn(title)
+
+
+for element in value_title:
+  for find_data in job:
+    if element.find(find_data) != -1 :
+      job[find_data] = job[find_data] + 1
+      break;
+
+print(job)
