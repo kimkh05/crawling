68730113b5ff76cca3a23483216c5fd03372b0be
kimkh05 <nestjs01@naver.com>
2022-12-30 10:26:54 +0900

    fix: delete import css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,11 @@
 import reactLogo from "./assets/react.svg";
 import InitCsv from "./components/init";
-import Wrapper from "./components/wrapper";
 import StyleProvider from "./style";
 
 function App() {
   return (
     <StyleProvider>
-      <Wrapper>
-        <span>Test</span>
-      </Wrapper>
+      <span>Test</span>
     </StyleProvider>
   );
 }
