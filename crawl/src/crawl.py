9181ee8c6d4c9bd8dda2a8ad59718c94e232c7cc
kimkh05 <nestjs01@naver.com>
2022-12-30 09:59:36 +0900

    refactor: divide file

--- crawl/src/crawl.py
+++ crawl/src/crawl.py
@@ -68,22 +68,3 @@ for element in value_title:
     if element.find(find_data) != -1 :
       job[find_data] = job[find_data] + 1
       break;
-
-job_title: str = ["Frontend", "Backend", "Android", "IOS", "OS", "Embedded", "Full-stack", "AI"]
-c = []
-for k, v in job.items():
-  c.append(v)
-
-cv = np.array(c)
-x = np.arange(len(c))
-colors = ['#ff9999', '#ffc000', '#8fd9b6', '#d395d0', "#A1B5DC", "#B6B6B6", "#3EAF0E", "#79DAE0"]
-wedgeprops = {'width': 0.7, 'edgecolor': 'w', 'linewidth': 5}
-plt.pie(cv, labels=job_title, autopct='%.1f%%', counterclock=False, startangle=260, colors=colors, wedgeprops=wedgeprops)
-
-plt.title("Developers")
-plt.show()
-
-plt.bar(x, c)
-plt.xticks(x, job_title, rotation=45)
-plt.title("Developers")
-plt.show()
---
+++ crawl/src/graph.py
@@ -0,0 +1,18 @@
+job_title: str = ["Frontend", "Backend", "Android", "IOS", "OS", "Embedded", "Full-stack", "AI"]
+c = []
+for k, v in job.items():
+  c.append(v)
+
+cv = np.array(c)
+x = np.arange(len(c))
+colors = ['#ff9999', '#ffc000', '#8fd9b6', '#d395d0', "#A1B5DC", "#B6B6B6", "#3EAF0E", "#79DAE0"]
+wedgeprops = {'width': 0.7, 'edgecolor': 'w', 'linewidth': 5}
+plt.pie(cv, labels=job_title, autopct='%.1f%%', counterclock=False, startangle=260, colors=colors, wedgeprops=wedgeprops)
+
+plt.title("Developers")
+plt.show() 
+
+plt.bar(x, c)
+plt.xticks(x, job_title, rotation=45)
+plt.title("Developers")
+plt.show()
