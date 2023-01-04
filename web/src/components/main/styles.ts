96801c8b3c2b82478af5a47488883893977f2f57
kimkh05 <nestjs01@naver.com>
2023-01-04 11:52:03 +0900

    feat: style

---
+++ web/src/components/main/styles.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10%;
+`;
+
+export const WebTitle = styled.span`
+  font-family: "Arita-dotum4.0(OTF)";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 32px;
+`;
+
+export const UnderLine = styled.hr`
+  width: 30%;
+  border: 1px solid #ffc700;
+  margin: 10px 0;
+`;
+
+export const InputCsvFiles = styled.input`
+  display: inline-block;
+  height: 40px;
+  padding: 0 10px;
+  vertical-align: middle;
+  border: 1px solid #dddddd;
+  width: 78%;
+  color: #999999;
+`;
+
+export const FileName = styled.span``;
